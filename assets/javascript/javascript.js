1502988a5d062a74cccc0e8e6c1cb4f3aa775a53
Calli Bonnet <calli@paradox.ai>
2020-01-09 21:20:24 -0700

    updating js file

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,59 +1,70 @@
+$(document).ready(function () {
 
-// Firebase Initialize 
-var firebaseConfig = {
-    apiKey: "AIzaSyDFPJZ3JZCCPlo-SvfJQMB0eq1yhoRj29w",
-    authDomain: "trainscheduler-calli.firebaseapp.com",
-    databaseURL: "https://trainscheduler-calli.firebaseio.com",
-    projectId: "trainscheduler-calli",
-    storageBucket: "trainscheduler-calli.appspot.com",
-    messagingSenderId: "868445978911",
-    appId: "1:868445978911:web:1ed4d57c0f2adf23b0b02a"
-};
+    // alert("aadded"); 
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database(); 
+    // Firebase Initialize 
+    var firebaseConfig = {
+        apiKey: "AIzaSyDFPJZ3JZCCPlo-SvfJQMB0eq1yhoRj29w",
+        authDomain: "trainscheduler-calli.firebaseapp.com",
+        databaseURL: "https://trainscheduler-calli.firebaseio.com",
+        projectId: "trainscheduler-calli",
+        storageBucket: "trainscheduler-calli.appspot.com",
+        messagingSenderId: "868445978911",
+        appId: "1:868445978911:web:1ed4d57c0f2adf23b0b02a"
+    };
 
-// Firebase Event Listener 
-database.ref().on('child_added', function(childSnapshot) {
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
 
-    var child = childSnapshot.val(); 
-    var trainNameDisplayed = childSnapshot.val().dbTrainName; 
-    var trainDestinationDisplayed = childSnapshot.val().dbDestination; 
-    var trainTimeDisplayed = childSnapshot.val().dbTrainTime; 
-    var trainFrequencyDisplayed = childSnapshot.val().dbFrequency; 
-    var trainNextArrivalDisplayed = childSnapshot.val().dbNextArrival; 
-    var trainMinutesAwayDisplayed = childSnapshot.val().dbMinutesAway; 
+    // Firebase Event Listener 
+    database.ref().on('child_added', function (childSnapshot) {
 
-    // moment.js 
-    var trainTimeConverted = moment(trainTimeDisplayed, "hh:mm"); 
-        var timeDifferance = moment().diff(moment(trainTimeConverted), "minutes"); 
-        var timeRemainder = timeDifferance % trainFrequencyDisplayed; 
-        var minsAway = trainFrequencyDisplayed - timeRemainder; 
-    var nextArrival = moment().add(minsAway, "minutes").format("hh:mm"); 
+        var child = childSnapshot.val();
+        var trainNameDisplayed = childSnapshot.val().dbTrainName;
+        var trainDestinationDisplayed = childSnapshot.val().dbDestination;
+        var trainTimeDisplayed = childSnapshot.val().dbTrainTime;
+        var trainFrequencyDisplayed = childSnapshot.val().dbFrequency;
+        var trainNextArrivalDisplayed = childSnapshot.val().dbNextArrival;
+        var trainMinutesAwayDisplayed = childSnapshot.val().dbMinutesAway;
 
-    $('#tbody').append('<tr><td>'+trainNameDisplayed+'</td><td>'+trainDestinationDisplayed+'</td><td>'+trainFrequencyDisplayed+'</td><td>'+nextArrival+'</td><td>'+minsAway+'</td></tr>'); 
-}); 
-// End of Firebase 
+        // moment.js 
+        var trainTimeConverted = moment(trainTimeDisplayed, "hh:mm");
+        var timeDifferance = moment().diff(moment(trainTimeConverted), "minutes");
+        var timeRemainder = timeDifferance % trainFrequencyDisplayed;
+        var minsAway = trainFrequencyDisplayed - timeRemainder;
+        var nextArrival = moment().add(minsAway, "minutes").format("hh:mm");
 
-// submit button listener
-$('#submitBtn').on('click', function() {
-    event.preventDefault(); 
+        $('#tbody').append('<tr><td>' + trainNameDisplayed + '</td><td>' + trainDestinationDisplayed + '</td><td>' + trainFrequencyDisplayed + '</td><td>' + nextArrival + '</td><td>' + minsAway + '</td></tr>');
+    });
+    // End of Firebase 
 
-    var newName = $('TrainName').val().trim(); 
-    var newDestination = $('TrainDestination').val().trim(); 
-    var newTrainTime = moment($('TrainTime').val().trim(), "H:mm").format("X"); 
-    var newFrequency = $('TrainFrequency').val().trim(); 
-     
-    // Clear values when sumbit is clicked 
-    $(this).closest('form').find("input[type=text], textarea").val(''); 
 
-    // Push new train information in the firebase database 
-    database.ref().push({
-        dbTrainName: newName, 
-        dbDestination: newDestination, 
-        dbTrainTime: newTrainTime, 
-        dbFrequency: newFrequency
-    }); 
-}); 
+
+
+
+    // submit button listener
+    $('#submitBtn').on('click', function () {
+        event.preventDefault();
+
+        var newName = $('#TrainName').val().trim();
+        var newDestination = $('#TrainDestination').val().trim();
+        // var newTrainTime = moment($('#TrainTime').val().trim(), "hh:mm").format("X");
+        var newFrequency = $('#TrainFrequency').val().trim();
+
+        // moment().format("hh:mm")
+
+        // Clear values when sumbit is clicked 
+        $(this).closest('form').find("input[type=text], textarea").val('');
+
+        // Push new train information in the firebase database 
+        database.ref().push({
+            dbTrainName: newName,
+            dbDestination: newDestination,
+            // dbTrainTime: newTrainTime,
+            dbFrequency: newFrequency
+        });
+    });
+});
+
 // end submit button listener
